a916c411ee9a2bf531adea2e1635a2ea3047bcff
백민정 <ggamji09@naver.com>
2026-03-17 15:20:04 +0900

    first commit

---
+++ src/test/java/dev/fisa/hateoas_practice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fisa.hateoas_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
